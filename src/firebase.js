a0bcab0122bd134ac4efafe9008db5e62247b57e
elizieb <elizieb.l3@gmail.com>
2021-08-22 00:09:11 -0300

    feat: adicioando .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const fireBaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCGV6veYzsc7LgT6tjbV0PuhNRRQb6xDHo",
-  authDomain: "chatfire-a6788.firebaseapp.com",
-  projectId: "chatfire-a6788",
-  storageBucket: "chatfire-a6788.appspot.com",
-  messagingSenderId: "647145096168",
-  appId: "1:647145096168:web:e2ed49e6301708469b9a5d",
-  measurementId: "G-WLJ7H1KZ1C",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 export const db = fireBaseApp.firestore();
